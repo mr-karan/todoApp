36ffe1086d0faac638ea5e2110b917a3a4c3e129
Dhiraj <dhiraj@dhiraj.in>
2015-11-26 20:52:34 +0530

    Error detected

--- app/src/main/java/com/example/dhiraj/todo/MainActivity.java
+++ app/src/main/java/com/example/dhiraj/todo/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -39,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView.invalidateViews();
 
         //working with the inflation
         toolbar = (Toolbar) findViewById(R.id.toolbarMain);
@@ -102,17 +98,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        listView.invalidateViews();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        listView.invalidateViews();
-    }
 
     @Override
     public void onBackPressed() {
